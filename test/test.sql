80c01d45d014baba5484817931682956a75dd058
Isaac Brodsky <isaac@isaacbrodsky.com>
2022-06-26 12:40:30 -0700

    add basic test script (#5)

---
+++ test/test.sql
@@ -0,0 +1,7 @@
+.load ./libh3ext
+select printf('%x', latLngToCell(0,0,0));
+select printf('%f', cellToLat(latLngToCell(0,0,0)));
+select printf('%f', cellToLng(latLngToCell(0,0,0)));
+select printf('%x', cellToParent(latLngToCell(0,0,5), 4));
+select printf('%d', getResolution(latLngToCell(0,0,5)));
+
